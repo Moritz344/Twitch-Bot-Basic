6fc9fbc285bef68c6f0da90d8b3e6c19df7c6627
root <root@moritz>
2024-09-28 16:38:05 +0200

    prüfen-auf-beleidigungen+befehle

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from twitchio.ext import commands
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+import json
 
 load_dotenv("secret.env")
 Twitch_token = os.getenv("TOKEN")
 channel = "pennti"
 
-
 class Bot(commands.Bot):
     def __init__(self):
+        with open("data.json","r",encoding="utf-8") as file:
+            bad_words_data = json.load(file)
+            self.bad_words = bad_words_data["BAD_WORDS"]
+
         super().__init__(token=Twitch_token,prefix="!",initial_channels=[channel])
+        self.date_now = datetime.now().strftime("%H:%M")
+
+
 
     async def event_ready(self):
         print(f"Bot ist bereit und verbunden mit {self.nick} im Kanal {channel}")
         self.count = 0
-    @commands.command(name="hello")
+    @commands.command(name="hallo")
     async def Hello(self,ctx):
         await ctx.send(f"Hallo, {ctx.author.name}")
 
@@ -29,21 +37,49 @@ class Bot(commands.Bot):
     @commands.command(name="heute")
     async def today(self,ctx):
         await ctx.send(f"Heute ehhhm ka {ctx.author.name}")
+
     @commands.command(name="lurk")
     async def lurker(self,ctx):
         await ctx.send(f"{ctx.author.name} ist im Lurk! MrDestructoid")
-    @commands.command(name="Texteditor")
+
+    @commands.command(name="editor")
     async def editor(self,ctx):
-        await ctx.send("Ich benutze LunarVim: https://www.lunarvim.org/ GlitchCat")
+        await ctx.send("Ich benutze Nvim: https://neovim.io/ GlitchCat")
+    
+    @commands.command(name="theme")
+    async def colorscheme(self,ctx):
+        await ctx.send("Ich benutze zurzeit oft: gruber-darker")
+
+  
 
     async def event_message(self,message):
-        if message.author is not None:
-            await self.handle_commands(message)
+        try:
+            if message.author is not None:
+                await self.handle_commands(message)
+            elif message.author is None:
+                return
+
+        except Exception as e:
+            print(e)
+
+        if message.author.name == self.nick:
+            return
+
+        for bad in self.bad_words:
+            if bad in message.content.lower():
+                await message.channel.send(f"NotLikeThis Bitte verwende keine schlechten Wörter! {message.author.name}")
+                return
+
+
         try:        
-            print(f"{message.author.name}: {message.content}")
+            print(f"[{self.date_now}] {message.author.name}: {message.content}")
+
         except AttributeError:
             print("User hat einen Befehl geschrieben!")
+        except Exception as e:
+            print(e)
+    
 
 if __name__ == "__main__":
-        bot = Bot()
-        bot.run()
+    bot = Bot()
+    bot.run()
